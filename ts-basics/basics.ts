c53250127de907dc35a8ce92303eae5bed213e81
ThomasCode92 <thomascode92@hotmail.com>
2023-05-08 12:54:53 +0200

    Working with Array & Object Types

--- ts-basics/basics.ts
+++ ts-basics/basics.ts
@@ -9,5 +9,12 @@ let isInstructor: boolean;
 isInstructor = false;
 
 // More complex types: arrays, objects
+let hobbies: string[];
+hobbies = ['Sports', 'Cooking'];
+
+let person: { name: string; age: number };
+person = { name: 'Thomas', age: 31 };
+
+let people: { name: string; age: number }[];
 
 // Function types, parameters
