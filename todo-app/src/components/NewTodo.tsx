0817fdcecc42aae073bfcfff8900141f8a739339
ThomasCode92 <thomascode92@hotmail.com>
2023-05-12 19:44:33 +0200

    Working with refs & useRef

--- todo-app/src/components/NewTodo.tsx
+++ todo-app/src/components/NewTodo.tsx
@@ -1,14 +1,19 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useRef } from 'react';
 
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+    if (enteredText.trim().length === 0) return;
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input id="text" type="text" />
+      <input id="text" type="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
