8d7a916421d98aa64c723c4a0bd962a8464cdc21
ThomasCode92 <thomascode92@hotmail.com>
2023-05-13 20:36:15 +0200

    Managing State & TypeScript

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
@@ -7,9 +7,12 @@ import './App.css';
 import Todo from './models/Todo';
 
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => {};
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos(prevTodos => prevTodos.concat(newTodo));
+  };
 
   return (
     <div>
